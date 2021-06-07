68080a56da37bd87ac4ccc84e17a1f388cfd39f1
Mohamed Hatem <mohamedhatemakl@gmail.com>
2021-06-07 23:04:24 +0200

    added addiyem and removeitem functionality to checkout item component

--- src/components/checkout-item/checkout-item.js
+++ src/components/checkout-item/checkout-item.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { clearItemFromCart } from "../../redux/cart/cart-actions";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart-actions";
 import "./checkout-item.scss";
 import { connect } from "react-redux";
 import { MdDelete } from "react-icons/md";
+import { AiFillMinusCircle } from "react-icons/ai";
+import { AiFillPlusCircle } from "react-icons/ai";
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
@@ -12,7 +18,14 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <AiFillMinusCircle
+          className="arrow"
+          onClick={() => removeItem(cartItem)}
+        />
+        <span className="value">{quantity}</span>
+        <AiFillPlusCircle className="arrow" onClick={() => addItem(cartItem)} />
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         <MdDelete />
@@ -23,6 +36,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
