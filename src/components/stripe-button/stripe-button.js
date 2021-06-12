ffa0da1e48582c3f1f7f87b360ef15be9ffa3962
Mohamed Hatem <mohamedhatemakl@gmail.com>
2021-06-12 18:46:15 +0200

    updating stripe integraion and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51J1Z4cGYlVhZmiLteoV5FzdQzzWtXOdShdO4vw1R4Dj5zFFD60Xc996rhpcOZMNjPO1GXaaGkKbAd6BmKUbcOFHQ00Ygu2xE7B";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-Commerce Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
